56b0ba3a5c898724aea159f881e2db67b1522d94
faraz117 <farazbhatti117@gmail.com>
2017-04-10 20:25:35 +0200

    Test Commit

--- app/src/main/java/com/electronicjunkies/sensorconnect/sensorconnect/MainActivity.java
+++ app/src/main/java/com/electronicjunkies/sensorconnect/sensorconnect/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        // Test
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
